02880009384e10b7cba2dac580c46e151653a54b
Jonathan <jondiehl22@gmail.com>
2024-08-17 16:38:58 -0400

    Handy Dandy Commit

--- src/main/java/io/zipcoder/crudapp/controller/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -1,17 +1,36 @@
 package io.zipcoder.crudapp.controller;
 
 import io.zipcoder.crudapp.model.Person;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.zipcoder.crudapp.service.PersonService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PersonController {
 
+    private PersonService service;
 
-    @PostMapping
-    public Person createPerson(Person person) {
-        return person;
+//    public PersonController(PersonService service) {
+//        this.service = service;
+//    }
 
+    @PostMapping("/people/")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        //Status code = HttpStatus.CREATED - Response: 201 Created
+        return new ResponseEntity<>(service.createPerson(person), HttpStatus.CREATED);
+    }
+
+    //{id} make it DyNAMIC - id of 1, id of 2.... and so on...
+    @GetMapping("/people/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable Integer id) {
+        return new ResponseEntity<>(service.getPerson(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/people/")
+    public ResponseEntity <Iterable<Person>> getPersonList() {
+        return new ResponseEntity<>(service.getPersonList(), HttpStatus.OK);
     }
 
 }
