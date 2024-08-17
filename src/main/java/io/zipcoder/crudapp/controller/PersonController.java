a8dfba5cde6ab024693e13cd703621102d7c84d9
Jonathan <jondiehl22@gmail.com>
2024-08-17 15:49:18 -0400

    Working through

---
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -0,0 +1,14 @@
+package io.zipcoder.crudapp.controller;
+
+import io.zipcoder.crudapp.model.Person;
+
+public class PersonController {
+
+
+
+    public void createPerson(Person person) {
+        return
+
+    }
+
+}
