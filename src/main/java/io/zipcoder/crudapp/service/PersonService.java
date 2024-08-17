4353a07344312b93c2c2ae6b426e5c1bae382b78
Jonathan <jondiehl22@gmail.com>
2024-08-17 16:14:34 -0400

    WIP

--- src/main/java/io/zipcoder/crudapp/service/PersonService.java
+++ src/main/java/io/zipcoder/crudapp/service/PersonService.java
@@ -4,11 +4,30 @@ import io.zipcoder.crudapp.model.Person;
 import io.zipcoder.crudapp.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service //tell Spring this class is going to be a service class
 //Service class handles BUSINESS logic for your application
 public class PersonService {
 
     private PersonRepository repository;
 
+    public Person createPerson(Person person) {
+        return repository.save(person);
+    }
 
+    public Person getPerson(Integer id) {
+        return repository.findOne(id);
+    }
+
+    public Iterable<Person> getPersonList() {
+        return repository.findAll();
+    }
+
+    public Person updatePerson(Integer id, Person person) {
+        Person person1 = repository.findOne(id);
+        person1.setFirstName(person1.getFirstName());
+        person1.setLastName(person1.getLastName());
+        return repository.save(person1);
+    }
 }
